ab804f2407f31d5d80d720fb2e58291e87354650
gbelda <gbelda56@gmail.com>
2021-10-29 12:34:41 +0200

    Copied old folder and changed js array to objects

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        counter: 0,
+        countries: [
+            {
+                country: 'Svezia',
+                countryImg: './img/01.jpg',
+                countryDescription: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                country: 'Svizzera',
+                countryImg: './img/02.jpg',
+                countryDescription: 'Lorem ipsum',
+            },
+            {
+                country: 'Gran Bretagna',
+                countryImg: './img/03.jpg',
+                countryDescription: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                country: 'Germania',
+                countryImg: './img/04.jpg',
+                countryDescription: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                country: 'Paradise',
+                countryImg: './img/05.jpg',
+                countryDescription: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ]
+
+    },
+    methods: {
+
+    }
+})
